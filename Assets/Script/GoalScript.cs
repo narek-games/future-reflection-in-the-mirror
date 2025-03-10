f4a99784b846cc63d1d1e62c847c317488e3936b
narek-games <ragunaroku2909@docomo.ne.jp>
2025-03-10 15:00:39 +0900

    future reflected in the mirror Ver. 0.2

---
+++ Assets/Script/GoalScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    public int scenenNum = 0;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        string stageName = "stage" + scenenNum;
+        // タグが"Player"のオブジェクトと衝突したとき
+        if (other.CompareTag("Player"))
+        {
+            // 次のステージに切り替える
+            SceneManager.LoadScene(stageName);
+        }
+    }
+}
--- Assets/Script/MirrorScript.cs
+++ Assets/Script/MirrorScript.cs
@@ -4,30 +4,55 @@ using System.Collections.Generic;
 
 public class MirrorScript : MonoBehaviour
 {
+    public GameObject[] flipObjects;
+    public GameObject[] flipVMirror;
+    public GameObject player;
+    // 状態(0->未設置(灰色)、1->鏡)を示す変数
+    public int mirrorState = 0;
+
+    public void OnTotched()
+    {
+        Debug.Log("タッチ検知");
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // タグが"Player"のオブジェクトと衝突したとき
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && mirrorState == 1)
         {
-            // 色相を反転(現状自身(鏡)の色を取得している(thisの部分を変更する))
-            Color tmpColor = this.GetComponent<SpriteRenderer>().color;
-            tmpColor.r = Mathf.Abs(tmpColor.r - 1.0f);
-            tmpColor.g = Mathf.Abs(tmpColor.g - 1.0f);
-            tmpColor.b = Mathf.Abs(tmpColor.b - 1.0f);
-            this.GetComponent<SpriteRenderer>().color = tmpColor;
+            // "Objects"タグを持つオブジェクトを同じ配列に入れる
+            flipObjects = GameObject.FindGameObjectsWithTag("Objects");
 
-            /*
-            foreach (GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+            // 配列から1つずつ順番に取り出し色相を反転
+            foreach(GameObject flipObj in flipObjects)
             {
-                if (obj.activeInHierarchy)
-                {
-                    Color tmpColor = obj.GetComponent<Color>();
-                    tmpColor.r = 1.0f - tmpColor.r;
-                    tmpColor.g = 1.0f - tmpColor.g;
-                    tmpColor.b = 1.0f - tmpColor.b;
-                    obj.GetComponent<Renderer>().material.color = tmpColor;
-                }
-            }*/
+                Color tmpColor = flipObj.GetComponent<SpriteRenderer>().color;
+                tmpColor.r = Mathf.Abs(tmpColor.r - 1.0f);
+                tmpColor.g = Mathf.Abs(tmpColor.g - 1.0f);
+                tmpColor.b = Mathf.Abs(tmpColor.b - 1.0f);
+                flipObj.GetComponent<SpriteRenderer>().color = tmpColor;
+            }
+
+            // "VMirror"タグを持つオブジェクトを同じ配列に入れる
+            flipVMirror = GameObject.FindGameObjectsWithTag("VMirror");
+
+            // 配列から1つずつ順番に取り出し色相を反転
+            foreach (GameObject flipObj in flipVMirror)
+            {
+                Color tmpColor = flipObj.GetComponent<SpriteRenderer>().color;
+                tmpColor.r = Mathf.Abs(tmpColor.r - 1.0f);
+                tmpColor.g = Mathf.Abs(tmpColor.g - 1.0f);
+                tmpColor.b = Mathf.Abs(tmpColor.b - 1.0f);
+                flipObj.GetComponent<SpriteRenderer>().color = tmpColor;
+            }
+
+            // プレイヤーの色相を反転
+            Color plaColor = player.GetComponent<SpriteRenderer>().color;
+            plaColor.r = Mathf.Abs(plaColor.r - 1.0f);
+            plaColor.g = Mathf.Abs(plaColor.g - 1.0f);
+            plaColor.b = Mathf.Abs(plaColor.b - 1.0f);
+            player.GetComponent<SpriteRenderer>().color = plaColor;
+
         }
     }
 }
