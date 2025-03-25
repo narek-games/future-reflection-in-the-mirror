3e376096eeab0b364dd3b8f0f19960c93f7e0654
narek-games <ragunaroku2909@docomo.ne.jp>
2025-03-25 14:58:48 +0900

    future reflected in the mirror Ver. 0.9

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,52 +4,84 @@ public class PlayerScript : MonoBehaviour
 {
     public GameManager gameManager;
 
-    // プレイヤーの初期色を保存する変数
-    Color plaFirstColor;
+    SpriteRenderer playerSpriteRenderer;
 
-    // プレイヤーの初期色の補色を保存する変数
-    Color plaCompColor;
+    // プレイヤーの画像8種類を格納する変数
+    public Sprite front;
+    public Sprite back;
+    public Sprite left;
+    public Sprite right;
+    public Sprite front_nega;
+    public Sprite back_nega;
+    public Sprite left_nega;
+    public Sprite right_nega;
 
     private void Start()
     {
-        plaFirstColor = this.GetComponent<SpriteRenderer>().color;
-        plaCompColor = new Color(Mathf.Abs(plaFirstColor.r - 1.0f), Mathf.Abs(plaFirstColor.g - 1.0f), Mathf.Abs(plaFirstColor.b - 1.0f));
+        playerSpriteRenderer= gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if(GameManager.worldState == 0)
-        {
-            this.GetComponent<SpriteRenderer>().color = plaFirstColor;
-        }
-        else if(GameManager.worldState == 1)
-        {
-            this.GetComponent<SpriteRenderer>().color = plaCompColor;
-        }
-
         if(gameManager.phase == 1)
         {
             // 左に移動
             if (Input.GetKey(KeyCode.LeftArrow))
             {
+                // 世界の状態によって見た目変更
+                if(GameManager.worldState == 0)
+                {
+                    playerSpriteRenderer.sprite = left;
+                }
+                else if(GameManager.worldState == 1)
+                {
+                    playerSpriteRenderer.sprite = left_nega;
+                }
                 this.transform.Translate(-0.005f, 0.0f, 0.0f);
             }
 
             // 右に移動
             if (Input.GetKey(KeyCode.RightArrow))
             {
+                // 世界の状態によって見た目変更
+                if (GameManager.worldState == 0)
+                {
+                    playerSpriteRenderer.sprite = right;
+                }
+                else if (GameManager.worldState == 1)
+                {
+                    playerSpriteRenderer.sprite = right_nega;
+                }
                 this.transform.Translate(0.005f, 0.0f, 0.0f);
             }
 
             // 上に移動
             if (Input.GetKey(KeyCode.UpArrow))
             {
+                // 世界の状態によって見た目変更
+                if (GameManager.worldState == 0)
+                {
+                    playerSpriteRenderer.sprite = back;
+                }
+                else if (GameManager.worldState == 1)
+                {
+                    playerSpriteRenderer.sprite = back_nega;
+                }
                 this.transform.Translate(0.0f, 0.005f, 0.0f);
             }
 
             // 下に移動
             if (Input.GetKey(KeyCode.DownArrow))
             {
+                // 世界の状態によって見た目変更
+                if (GameManager.worldState == 0)
+                {
+                    playerSpriteRenderer.sprite = front;
+                }
+                else if (GameManager.worldState == 1)
+                {
+                    playerSpriteRenderer.sprite = front_nega;
+                }
                 this.transform.Translate(0.0f, -0.005f, 0.0f);
             }
         }
