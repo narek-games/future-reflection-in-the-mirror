e771af39fe6eb6205b0dcdd5e662da4a29a5bae9
narek-games <ragunaroku2909@docomo.ne.jp>
2025-04-15 15:00:04 +0900

    future reflected in the mirror Ver. 0.19

---
+++ Assets/Script/ParticleColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ParticleColorChanger : MonoBehaviour
+{
+    [SerializeField] ParticleSystem particle;
+    [SerializeField] Color color;
+    [SerializeField] Color nega_color;
+
+    void Update()
+    {
+        var main = particle.main;
+
+        if (GameManager.worldState == 0)
+        {
+            main.startColor = new ParticleSystem.MinMaxGradient(color);
+        }
+        else if (GameManager.worldState == 1)
+        {
+            main.startColor = new ParticleSystem.MinMaxGradient(nega_color);
+        }
+    }
+}
